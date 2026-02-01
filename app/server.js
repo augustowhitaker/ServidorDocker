1fc26579ae4e67d40882cd139f8e062be9b352d6
augustowhitaker <awhitakerflx@gmail.com>
2026-01-31 22:00:58 -0300

    s

---
+++ app/server.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/health', (req, res) => res.json({ ok: true }));
+
+app.listen(PORT, () => {
+  console.log(`Painel rodando em http://localhost:${PORT}`);
+});
